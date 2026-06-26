0e8220c8665cbeafd3ae2cb79068a4e67ad712b0
Bruno1906-bg <2402137@utrivieramaya.edu.mx>
2026-06-25 20:38:56 -0500

    Cambios deploy backend

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CDN_NAME,
+    api_key: process.env.CDN_KEY,
+    api_secret: process.env.CDN_SECRET
 });
 
 app.use(cors({
